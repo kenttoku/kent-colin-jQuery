4cc5954db6507eff007f301bfbbcce461460e62f
Kent Tokunaga <kenttokunaga@gmail.com>
2018-08-23 15:44:00 -0400

    adding items and checking items

---
+++ kent-colin-shopping-list/index.js
@@ -0,0 +1,44 @@
+// enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+// check and uncheck items on the list by clicking the "Check" button
+// permanently remove items from the list
+
+// function() {
+  
+// }
+
+function addItem(entry) {
+  const htmlElement = `
+  <li>
+    <span class="shopping-item">${entry}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>
+  `;
+
+  $('.shopping-list').append(htmlElement);
+}
+
+function main() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+
+    const entry = $('.js-shopping-list-entry').val();
+    addItem(entry);
+  });
+
+  $('.shopping-list').on('click', 'button.shopping-item-toggle', function(event) {
+    // select the food item
+    // toggle span class
+    const target = $(event.currentTarget).closest('li').find('span.shopping-item');
+    target.toggleClass('shopping-item__checked');
+    console.log(target);
+  });
+}
+
+$(main);
