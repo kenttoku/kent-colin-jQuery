e9b3b50dbaccf6fe7a7b985b61b8403050c81e8d
Colin Rupp <colin@thinkful.com>
2018-08-23 11:41:39 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('hi');
+
+function selectCat() {
+  $('.thumbnail').on('click', event => {
+    const largeCat = $(event.currentTarget).find('img').attr('src');
+    const largeCatAlt = $(event.currentTarget).find('img').attr('alt');
+    $('div.hero img').attr('src', largeCat);
+    
+    $('div.hero img').attr('alt', largeCatAlt);
+  })
+  
+}
+
+$(selectCat);
+
+
+//clear div.hero html
+//insert largecat into div.hero
+
